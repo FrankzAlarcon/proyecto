490e923a57e3f1c6e98252e3bf82dc61f8ec1689
dell <dell@DESKTOP-AP4BT08>
2021-12-23 22:08:43 -0500

    Version Inicial

--- src/proyecto/Proyecto.java
+++ src/proyecto/Proyecto.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package proyecto;
 
 /**
  *
- * @author Frankz
+ * @author Frankz el máquina
  */
 public class Proyecto {
 
@@ -15,7 +11,7 @@ public class Proyecto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
